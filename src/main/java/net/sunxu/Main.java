b73a2aa64fb7839ec8d15f381cfcf93feb8ff5c9
ntsunxu <sunnetworks@126.com>
2019-07-23 12:25:20 +0800

    Update Main.java

--- src/main/java/net/sunxu/Main.java
+++ src/main/java/net/sunxu/Main.java
@@ -2,6 +2,6 @@ package net.sunxu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world 2");
     }
 }
